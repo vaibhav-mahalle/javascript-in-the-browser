b649592e13a6071a9fa57139cb902dda174e66f3
Vaibhav Kishor Mahalle <vaibhavmahalle95@gmail.com>
2021-08-27 13:07:18 +0530

    dothraki translation completed

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 var btnTranslate = document.querySelector("#btn");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+
+var URL = "https://api.funtranslations.com/translate/dothraki.json";
+
+function urlGenerator(input)
+{
+    var finalURL = URL + "?text=" + input;
+    return finalURL;
+}
+
 
 function clickEventHandle()
 {
-    console.log("clicked!");
+    var input1 = txtInput.value ;
+
+    fetch(urlGenerator(input1))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        txtOutput.innerText = translatedText; // output
+       })
+    
 }
 
 btnTranslate.addEventListener("click",clickEventHandle)
